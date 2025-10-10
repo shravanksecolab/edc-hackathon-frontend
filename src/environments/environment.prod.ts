2ce1dd4dc6b64c052bc6a21f621446ea015551cf
nikhil.r <nikhil.r@ecolab.com>
2025-10-10 16:03:35 +0530

    uri update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
         auth: {
             clientId: "99048e17-d7cd-475f-a561-2b348e70da91",
             authority: "https://login.microsoftonline.com/c1eb5112-7946-4c9d-bc57-40040cfe3a91",
-            redirectUri: "https://your-app-name.azurestaticapps.net", // Replace with your actual URL
+            redirectUri: "https://polite-ocean-0ac84860f.2.azurestaticapps.net", // Replace with your actual URL
         }
     }
 };
