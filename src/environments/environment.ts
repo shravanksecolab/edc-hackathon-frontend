c05b6e671a0c0b343e79f3eaee7e27ce3da99b91
Shravan K Subrahmanya <shravan.ksubrahmanya@ecolab.com>
2025-10-13 17:32:16 +0530

    [update] changed redirect uri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
         auth: {
             clientId: "99048e17-d7cd-475f-a561-2b348e70da91",
             authority: "https://login.microsoftonline.com/c1eb5112-7946-4c9d-bc57-40040cfe3a91",
-            redirectUri: "http://localhost:4200",
+            redirectUri: "https://brave-desert-06ee8da0f.2.azurestaticapps.net",
         }
     }
 };
